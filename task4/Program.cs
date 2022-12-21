713d7a2a27163255b1f163ffcd3f6a15450ba8a2
Nataliya <arnicaaliya@gmail.com>
2022-12-21 20:02:31 +0300

    tast 4_Max

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,6 +2,16 @@
 int a= Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите второе число");
 int b= Convert.ToInt32(Console.ReadLine());
-int max=Math.Max(a,b);
-int min=Math.Min(a,b);
-System.Console.WriteLine($"Максимальное число {max},Минимальное число {min}");
+System.Console.WriteLine("Введите третье число");
+int c= Convert.ToInt32(Console.ReadLine());
+
+int max=a;
+if (max<b)
+{
+    max=b;
+}
+
+if (max<c)
+{
+    max=c;
+}
