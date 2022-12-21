27536231bdc4f072e1541f263357c025b2156891
Nataliya <arnicaaliya@gmail.com>
2022-12-21 17:51:18 +0300

    version with Math.Max

--- task2.1/Program.cs
+++ task2.1/Program.cs
@@ -2,11 +2,5 @@
 int a= Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите второе число");
 int b= Convert.ToInt32(Console.ReadLine());
-if (a>b)
-{
-    System.Console.WriteLine("Максимальное число "+a);
-}
-else
-{
-    System.Console.WriteLine("Максимальное число "+b);
-}
+int max=Math.Max(a,b);
+System.Console.WriteLine($"Максимальное число {max}");
