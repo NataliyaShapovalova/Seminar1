e4271ef5ab7934c0204e591204831320265edc83
Nataliya <arnicaaliya@gmail.com>
2022-12-21 17:18:54 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите первое число");
+int a= Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int b= Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    System.Console.WriteLine("Максимальное число -"+a);
+}
+else
+{
+    System.Console.WriteLine("Максимальное число -"+b);
+}
