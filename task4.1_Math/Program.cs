290516912271c3d3a18c0d9d83122ed1606e1aad
Nataliya <arnicaaliya@gmail.com>
2022-12-21 20:23:12 +0300

    Math.max

---
+++ task4.1_Math/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите первое число");
+int a= Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int b= Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число");
+
+int c= Convert.ToInt32(Console.ReadLine());
+int max=Math.Max(a,Math.Max(b,c));
+
+System.Console.WriteLine($"Максимальное число {max}");
