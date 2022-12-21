918b4726e4d4d82ec5bcff49a6e4dd6059a1d187
Nataliya <arnicaaliya@gmail.com>
2022-12-21 22:28:59 +0300

    четные числа

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,13 +1,11 @@
 ﻿System.Console.WriteLine("Введите число");
 int N=Convert.ToInt32(Console.ReadLine());
-int i=1;
 
 for (int i = 1; i < N; i++)
 {
    if (i%2==0)
    {
-    System.Console.Write($"Все четные числа От 1 до+{N}"i+"");
-
+        System.Console.Write (i+" ");
    } 
 }
 
