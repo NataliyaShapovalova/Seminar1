c914e0683eea40229f521a8ee59a1f134f3eda74
Nataliya <arnicaaliya@gmail.com>
2022-12-21 21:12:03 +0300

    числа

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите число для проверки на четность");
+int a= Convert.ToInt32(Console.ReadLine());
+if (a%2==0)
+{
+    System.Console.WriteLine("Введенное число является четным");
+}
+else
+{
+    System.Console.WriteLine("Введенное число является  не четным");
+}
---
+++ task8/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите число");
+int N=Convert.ToInt32(Console.ReadLine());
+int i=1;
+if (N<=1)
+{
+    System.Console.WriteLine("Введите число больше чем 1");
+}
+else
+{
+    System.Console.WriteLine("Все числа от 1 до"+N);
+    while (i<N+1)
+    {
+        System.Console.Write (i+",");
+        i++
+    }
+}
